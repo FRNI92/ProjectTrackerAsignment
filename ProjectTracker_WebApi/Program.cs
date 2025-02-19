711a7d05954284244960962f4e3e8959f8f2ccb8
fredrik <freedrik92@hotmail.com>
2025-02-19 15:05:16 +0100

    Deletet the test for baserepository since its an abstract class and cant be instansiated

--- ProjectTracker_WebApi/Program.cs
+++ ProjectTracker_WebApi/Program.cs
@@ -30,3 +30,31 @@
 //app.MapControllers();
 //app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 //app.Run();
+
+
+
+
+
+
+
+
+//controller
+//var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddControllers();
+//builder.Services.AddOpenApi();
+
+//builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("Data Source=localhost;Initial Catalog=sql_database;Integrated Security=True;Encrypt=True;Trust Server Certificate=True"));
+
+
+//builder.Services.AddScoped<EmployeeService>();
+//builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+//var app = builder.Build();
+//app.MapOpenApi();
+//app.UseHttpsRedirection();
+//app.UseAuthorization();
+//app.MapControllers();
+//app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+//app.Run();
+
+//controller
